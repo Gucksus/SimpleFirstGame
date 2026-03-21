f296da9ddfefc294634932b98c0cbd17d779e3ba
Gucksus <130300776+Gucksus@users.noreply.github.com>
2026-03-21 21:15:27 +0700

    Initial commit

---
+++ core/src/main/java/io/github/gucksus/simplefirstgame/Core.java
@@ -0,0 +1,32 @@
+package io.github.gucksus.simplefirstgame;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class Core extends ApplicationAdapter {
+    Texture background;
+    private SpriteBatch batch;
+
+
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+    }
+
+    @Override
+    public void render() {
+        ScreenUtils.clear(Color.BLACK);
+        batch.begin();
+
+        batch.end();
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+    }
+}

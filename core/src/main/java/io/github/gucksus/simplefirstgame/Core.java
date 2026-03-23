7fe64d29ca9d975a1340ad68a7de4c08262b1776
Gucksus <130300776+Gucksus@users.noreply.github.com>
2026-03-23 11:29:37 +0700

    Basic ship and bullet .HUGE commit because of my stupidity. I didn't make any commit until they piled up and the order is just everywhere ToT.

--- core/src/main/java/io/github/gucksus/simplefirstgame/Core.java
+++ core/src/main/java/io/github/gucksus/simplefirstgame/Core.java
@@ -1,32 +1,41 @@
 package io.github.gucksus.simplefirstgame;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.FitViewport;
+import io.github.gucksus.simplefirstgame.entities.Bulletlv1;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Core extends ApplicationAdapter {
     // Declare variables.
     Texture backgroundTexture;
     Texture shipTexture;
+    Texture bulletlv1Texture;
     private SpriteBatch batch;
     Sprite shipSprite;
     Sprite backgroundSprite;
     FitViewport viewport;
+    Array <Bulletlv1> bulletlv1Array;
 
     @Override
     public void create() {
         // Adds texture.
         backgroundTexture = new Texture("background.png");
         shipTexture = new Texture("ShipSprite.png");
+        bulletlv1Texture = new Texture("bullet_texture.png");
 
         // Initialize sprites.
         shipSprite = new Sprite(shipTexture);
         shipSprite.setSize(1, 1);
+        shipSprite.setCenterX(4);
         backgroundSprite = new Sprite(backgroundTexture);
         // Height is 22 because the background is supposed to be twice the size to create infinity effect.
         backgroundSprite.setSize(8, 22);
@@ -34,6 +43,7 @@ public class Core extends ApplicationAdapter {
         // Initialize sprite batch and viewport.
         batch = new SpriteBatch();
         viewport = new FitViewport(8,11);
+        bulletlv1Array = new Array<>();
     }
 
     @Override
@@ -45,16 +55,51 @@ public class Core extends ApplicationAdapter {
     @Override
     public void render() {
         input();
-        logic();
+        clampLogic();
+        bulletLogic();
         draw();
     }
 
     private void input() {
+        float speed = 6f;
+        float delta = Gdx.graphics.getDeltaTime();
 
+        if (Gdx.input.isKeyPressed(Input.Keys.A)) {
+            shipSprite.translateX(-speed * delta);
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.D)) {
+            shipSprite.translateX(speed * delta);
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.W)) {
+            shipSprite.translateY(speed * delta);
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.S)) {
+            shipSprite.translateY(-speed * delta);
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.J)) {
+            bulletSpawn();
+        }
     }
 
-    private void logic() {
+    private void bulletSpawn() {
+        // Maybe this line will be buggy.
+        float iniX = shipSprite.getX();
+        float iniY = shipSprite.getY();
+        bulletlv1Array.add(new Bulletlv1(bulletlv1Texture, iniX, iniY));
+    }
 
+    private void bulletLogic() {
+        for (Bulletlv1 bullet: bulletlv1Array) {
+            bullet.update(Gdx.graphics.getDeltaTime());
+        }
+    }
+
+    private void clampLogic() {
+        float worldHeight = viewport.getWorldHeight();
+        float worldWidth = viewport.getWorldWidth();
+
+        shipSprite.setX(MathUtils.clamp(shipSprite.getX(), -(shipSprite.getWidth()/4), worldWidth - shipSprite.getWidth() / 4 * 3));
+        shipSprite.setY(MathUtils.clamp(shipSprite.getY(), 0, worldHeight - shipSprite.getHeight()));
     }
 
     private void draw() {
@@ -68,6 +113,10 @@ public class Core extends ApplicationAdapter {
         // Draw background and ship.
         backgroundSprite.draw(batch);
         shipSprite.draw(batch);
+
+        for (Bulletlv1 bulletlv1: bulletlv1Array) {
+            bulletlv1.bulletSelfSprite.draw(batch);
+        }
 
         batch.end();
     }
---
+++ core/src/main/java/io/github/gucksus/simplefirstgame/entities/Bulletlv1.java
@@ -0,0 +1,21 @@
+package io.github.gucksus.simplefirstgame.entities;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+
+public class Bulletlv1 {
+    final float speed = 5f;
+//    final float damage = 1;
+    public Sprite bulletSelfSprite;
+
+    public Bulletlv1(Texture texture, float iniX, float iniY) {
+        bulletSelfSprite = new Sprite(texture);
+        bulletSelfSprite.setSize(1, 1);
+        bulletSelfSprite.setCenterX(iniX);
+        bulletSelfSprite.setY(iniY);
+    }
+
+    public void update(float delta){
+        bulletSelfSprite.translateY(delta * speed);
+    }
+}
